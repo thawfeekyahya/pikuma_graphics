888bf3bb85f2cf588b5186cede221f54434ec983
Thawfeek Yahya <thawfeek_yahya@yahoo.co.in>
2025-10-11 12:27:14 +0530

    Adding SDLWindow

--- modules/include/sdlwindow.h
+++ modules/include/sdlwindow.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <SDL2/SDL.h>
 
 class SdlWindow {
 public:
-    SdlWindow();
+     SdlWindow();
+     SDL_Window* window;
+     SDL_Renderer* renderer;
+     bool isRunning() const;
+private:
+     bool m_isRunning = false;
 
 };
--- modules/src/sdlwindow.cpp
+++ modules/src/sdlwindow.cpp
@@ -11,4 +11,31 @@ SdlWindow::SdlWindow() {
     } else {
         cout<<"Success"<<endl;
     }
+
+    //Create SDL window
+    window = SDL_CreateWindow(NULL,SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              800,600,      
+                              SDL_WINDOW_BORDERLESS);
+
+    if (window == nullptr) {
+       cout<<"Error creating SDL window"<<endl;
+       m_isRunning = false;
+    }
+
+    //Create SDL Renderer
+    renderer = SDL_CreateRenderer(window,-1,0);
+
+    if (renderer == nullptr) {
+        cout<<"Error creating SDL Renderer"<<endl;
+        m_isRunning = false;
+    }
+
+    m_isRunning = true;
+}
+
+
+
+bool SdlWindow::isRunning() const {
+    return m_isRunning;
 }
